ae4798776acc9b3c3b702e735e5132b28c98e47e
maeldun-x <m@mdoyle.com>
2015-04-12 10:22:53 -0400

    More changes to ensure i didn't forget them

--- src/com/ibm/cloudoe/ecaas/samples/AsteroidBuilder.java
+++ src/com/ibm/cloudoe/ecaas/samples/AsteroidBuilder.java
@@ -1,14 +1,20 @@
 package com.ibm.cloudoe.ecaas.samples;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.io.FileUtils;
 
 import com.ancientprogramming.fixedformat4j.format.FixedFormatManager;
 import com.ancientprogramming.fixedformat4j.format.impl.FixedFormatManagerImpl;
-import com.ibm.json.java.JSONObject;
 
 public class AsteroidBuilder {
 
@@ -32,34 +38,55 @@ public class AsteroidBuilder {
 		System.out.println("Response Code : " + responseCode);
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		
 		String inputLine;
 		
 		while ((inputLine = in.readLine()) != null) {
 			//parse the line
 			MinorPlanetLine record = manager.load(MinorPlanetLine.class, inputLine);
-
-//		    System.out.println("The name : " + record.getName());
-//		    System.out.println("The id : " + record.getId());
-//		    System.out.println("The mag : " + record.getMagnitude());
-
-		    //System.out.println("Exported: " + manager.export(record));
 		    
 		    Asteroid theAsteroid = new Asteroid(record);
-		    
 		    
 		    asteroids.put(theAsteroid.getName(), theAsteroid);
 		}
 		
-		
-
 	    in.close();
 
-			
+	    asteroids = enrichWithThreatInfo(asteroids);
+	    
 		return asteroids;
 	}
 
 	
+	private HashMap enrichWithThreatInfo(HashMap asteroids) throws Exception{
+		// load the enrichment data
+		//FileUtils.writeStringToFile(new File("YAGOTH.txt"), "SCREAMM!");
+		BufferedReader in = new BufferedReader(new FileReader(new File("WebContent/impactdatalite.csv")));
+		String inputLine = null;
+		
+		while ((inputLine = in.readLine()) != null) {
+			//parse the line
+			System.out.println(inputLine);
+			String[] data= inputLine.split(",");
+			
+			//shove it into a map
+			
+		    
+		}
+		
+		// Enrich the asteroids
+		Iterator iterator = asteroids.entrySet().iterator();
+		while(iterator.hasNext()){
+		   Map.Entry entry = (Entry) iterator.next();
+		   //System.out.printf("Key : %s and Value: %s %n", entry.getKey(), entry.getValue());
+		   // pick the corresponding entry from the enrichment data
+		   
+		   // add it to the asteroid's map
+		}
+		
+		return asteroids;
+	}
+
+
 	protected String sendGet(String theURL) throws Exception {
 
 		String url = theURL;
